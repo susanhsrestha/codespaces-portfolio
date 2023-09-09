e0146b930cebd518575d6e52b16bcc6f792916af
Susan Shrestha <thesusanshrestha@gmail.com>
2023-09-09 17:23:59 +0000

    changes in the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Susan Shrestha",
+  title: "Computer Engineering Student",
+  email: "thesusanshrestha@gmail.com",
+  gitHub: "susanhsrestha",
+  instagram: "susanhsrestha",
+  linkedIn: "susanhsrestha",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "susanhsretha",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
